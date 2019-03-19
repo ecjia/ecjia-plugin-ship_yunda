9a5c79298bd61dd2bc7e972dfcd9362cf57a7e57
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-19 17:35:32 +0800

    插件头部信息添加Text Domain参数

--- ship_yunda.php
+++ ship_yunda.php
@@ -52,6 +52,7 @@ Author: ECJIA TEAM
 Version: 2.0.0
 Author URI: http://www.ecjia.com/
 Plugin App: shipping
+Text Domain: ship_yunda
 */
 defined('IN_ECJIA') or exit('No permission resources.');
 class plugin_ship_yunda {
