34f07205e176e38868a6a464d3bfad8545feb27c
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-19 17:25:49 +0800

    更新插件地址

--- ship_yunda.php
+++ ship_yunda.php
@@ -46,7 +46,7 @@
 //
 /*
 Plugin Name: 韵达速递
-Plugin URI: http://www.ecjia.com/plugins/ecjia.yunda/
+Plugin URI: http://www.ecjia.com/plugins/ecjia.ship_yunda/
 Description: 韵达速递，保价费按照申报价值的3％交纳，未保价的物品遗失，按照物品的实际价格予以赔偿,但最高不超过所收运费的3倍；物品保价金额最高不超过2万元。
 Author: ECJIA TEAM
 Version: 2.0.0
